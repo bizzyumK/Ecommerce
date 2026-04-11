0174fa4595f3a61646e10600aaf1ed86560ed205
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-11 17:04:19 +0545

    updated

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -23,6 +23,7 @@ export async function getProductById(req: Request, res: Response) {
 }
 
 export async function getAllProduct(req: Request, res: Response) {
+                console.log("GET ALL PRODUCTS HIT");
     try {
         const products = await Product.find();
         return res.status(200).json({
