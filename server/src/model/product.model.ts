f1c844adb735c41c3b5b17152f90c44fb108593a
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-11 16:36:21 +0545

    fix: product model

--- server/src/model/product.model.ts
+++ server/src/model/product.model.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        name: String,
-        price: Number,
-        description: String,
-        images: [String],
-        sizes: [String],
-        stock: Number,
-        category: String,
+        name: { type: String, required: true, },
+        price: { type: Number, required: true, },
+        description: { type: String, required: true, },
+        images: { type: [String], default: [], },
+        sizes: { type: [String], default: [], },
+        stock: { type: Number, default: 0, },
+        category: { type: String, required: true, },
     },
     { timestamps: true }
 );
