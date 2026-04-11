dfc5b5a1f78b33e2d757a577757c6a295fbea9d0
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-11 16:45:27 +0545

    feat: delete product

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -106,5 +106,23 @@ export async function updateProduct(req: Request, res: Response) {
         });
     }
 }
-export function deleteProduct(req: Request, res: Response) {
+
+export async function deleteProduct(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if (!deletedProduct) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Product deleted successfully"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Invalid ID or server error"
+        });
+    }
 }
