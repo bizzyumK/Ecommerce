3984d4afb02ef7680f29d4ccce6b0c06ecfa0060
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-12 07:32:20 +0545

    so far so good

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -1,6 +1,33 @@
 import { Request, Response } from "express";
 
 export function createOrder(req: Request, res: Response) {
+    try {
+        const { items, address } = req.body;
+        if (!items) {
+            return res.status(404).json({
+                message: "Atleast one item is required for checkout",
+                status: false
+            });
+        }
+        if (!address) {
+            return res.status(404).json({
+                message: "Address is required",
+                status: false
+            });
+        }
+        if (!req.user) {
+            return res.status(403).json({
+                message: "Unauthorized, must login",
+                status: false
+            });
+        }
+        const user = req.user?.id;
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Invalid ID or server error"
+        });
+    }
 }
 export function getUserOrder(req: Request, res: Response) {
 }
