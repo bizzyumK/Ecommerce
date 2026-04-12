54dac4f377b25d1644782de9417bd18ad2e82d9f
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-12 17:35:27 +0545

    set baseURL for backend connection

---
+++ client/src/api/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const API = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+});
+
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
