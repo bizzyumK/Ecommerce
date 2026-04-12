8d359c768aa063d1db4d160d1175cf7b5ad7df6b
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-12 21:47:45 +0545

    nav bar modified

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<AuthProvider>
 				<CartProvider>
+					<Navbar/>
 					<AppRoutes />
 				</CartProvider>
 			</AuthProvider>
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Navbar() {
+    const { cart } = useContext(CartContext);
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
+    return (
+        <nav className="shadow-sm">
+            <div className="max-w-[1420px] m-auto px-2 py-4 flex justify-between items-center">
+                <Link to="/" className="text-xl font-bold">
+                    Sajilo Style 🛍️
+                </Link>
+                <div className="flex gap-6 items-center">
+                    <Link to="/" className="hover:text-gray-300">Home</Link>
+                    <Link to="/" className="hover:text-gray-300">Product</Link>
+                    <Link to="/cart" className="hover:text-gray-300 relative">
+                        Cart
+                        {cart.length > 0 && (
+                            <span className="ml-1 bg-red-500 text-xs px-2 py-0.5 rounded-full">
+                                {cart.length}
+                            </span>
+                        )}
+                    </Link>
+
+                    {!user ? (
+                        <>
+                            <Link to="/login" className="hover:text-gray-300">Login</Link>
+                        </>
+                    ) : (
+                        <>
+                            <span className="text-sm text-gray-300"> Hi, {user.username || "User"}</span>
+                            <button
+                                onClick={handleLogout}
+                                className="bg-red-500 px-3 py-1 rounded"
+                            >
+                                Logout
+                            </button>
+                        </>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
+}
