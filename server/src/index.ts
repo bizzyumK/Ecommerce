e2375b11171361b7bd6af6db1e2bd5e80cfbe327
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-13 11:17:25 +0545

    cors allowed

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,17 @@ import { connectDB } from './config/db';
 import auth from './routes/auth.routes';
 import product from './routes/product.routes';
 import order from './routes/order.routes';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+const allowedOrigin = {
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,POST, PUT, DELETE',
+    credentials: true
+};
+app.use(cors(allowedOrigin));
 connectDB();
 
 app.use('/api/auth', auth);
