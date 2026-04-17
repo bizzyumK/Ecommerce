8ccda1e525a07dd7293635374773c7a5b56ad760
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-17 06:49:35 +0545

    fix: on refresh user state reset

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,9 +1,27 @@
 import { createContext, useState } from "react";
+import { useEffect } from "react";
+import { API } from "../api/axios";
 
 //To store user and token globally
 export const AuthContext = createContext<any>(null);
 
 export const AuthProvider = ({ children }: any) => {
+    useEffect(() => {
+        const fetchUser = async () => {
+            const token = localStorage.getItem("token");
+
+            if (!token) return;
+
+            try {
+                const res = await API.get("/auth/me");
+                setUser(res.data);
+            } catch (err) {
+                logout(); // invalid token
+            }
+        };
+
+        fetchUser();
+    }, []);
     const [user, setUser] = useState(null);
     const login = (data: any) => {
         localStorage.setItem("token", data.accessToken);
@@ -14,6 +32,7 @@ export const AuthProvider = ({ children }: any) => {
         localStorage.removeItem("token");
         setUser(null);
     };
+                console.log(user);
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
