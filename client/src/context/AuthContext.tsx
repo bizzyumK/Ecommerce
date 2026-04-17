f3d7990990f7910b4110e1d0fe31f605dd4d2141
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-17 06:49:52 +0545

    fix: on refresh user state reset

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,38 +1,50 @@
-import { createContext, useState } from "react";
-import { useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { API } from "../api/axios";
 
-//To store user and token globally
 export const AuthContext = createContext<any>(null);
 
 export const AuthProvider = ({ children }: any) => {
-    useEffect(() => {
-        const fetchUser = async () => {
-            const token = localStorage.getItem("token");
-
-            if (!token) return;
-
-            try {
-                const res = await API.get("/auth/me");
-                setUser(res.data);
-            } catch (err) {
-                logout(); // invalid token
-            }
-        };
-
-        fetchUser();
-    }, []);
-    const [user, setUser] = useState(null);
-    const login = (data: any) => {
-        localStorage.setItem("token", data.accessToken);
-        setUser(data);
-    };
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
 
     const logout = () => {
         localStorage.removeItem("token");
         setUser(null);
     };
-                console.log(user);
+
+    const login = (data: any) => {
+        localStorage.setItem("token", data.accessToken);
+        setUser({
+            id: data.id,
+            email: data.email,
+            isAdmin: data.isAdmin,
+        });
+    };
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const res = await API.get("/auth/me");
+                setUser(res.data);
+            } catch (err) {
+                logout(); // token invalid
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    if (loading) {
+        return <div className="text-center mt-20">Loading...</div>;
+    }
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
