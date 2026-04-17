836523e3be7a46bdf24c20d1a3c0439cec4bb118
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-17 07:15:37 +0545

    feat: guest route added

---
+++ client/src/routes/GuestRoute.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+function GuestRoute({ children }: Props) {
+    const { user } = useContext(AuthContext);
+
+    if (user) {
+        return <Navigate to="/" replace />;
+        //with replace user wont' be able to use back button to returns to login again 
+    }
+
+    return children;
+}
+
+export default GuestRoute;
