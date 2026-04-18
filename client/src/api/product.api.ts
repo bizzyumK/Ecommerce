d429c7045b31d132260f048d03b71c596f7941f7
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-18 22:14:35 +0545

    product api fixed

--- client/src/api/product.api.ts
+++ client/src/api/product.api.ts
@@ -5,7 +5,7 @@ export const getProducts = () => API.get("/product");
 export const getProductById = (id: string) =>
     API.get(`/product/${id}`);
 
-export const createProduct = () => API.post("/product");
+export const createProduct = (data: object) => API.post("/product", data);
 
 export const updateProduct = (id: string, data: object) =>
     API.put(`/product/${id}`, data);
