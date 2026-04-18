2aaa322f33cb257b6c2c253eae068c99f20953d3
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-18 19:27:12 +0545

    product api updated

--- client/src/api/product.api.ts
+++ client/src/api/product.api.ts
@@ -4,3 +4,11 @@ export const getProducts = () => API.get("/product");
 
 export const getProductById = (id: string) =>
     API.get(`/product/${id}`);
+
+export const createProduct = () => API.post("/product");
+
+export const updateProduct = (id: string) =>
+    API.put(`/product/${id}`);
+
+export const deleteProduct = (id: string) =>
+    API.delete(`/product/${id}`);
