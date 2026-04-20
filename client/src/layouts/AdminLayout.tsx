42612750c72191c890eaa424caf0e489daad5210
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-20 18:33:46 +0545

    fix: layout icons added

--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -13,31 +13,34 @@ export default function AdminLayout() {
                 <div className="flex flex-col gap-2 text-sm">
                     <button
                         onClick={() => navigate("/admin")}
-                        className={`text-left px-3 py-2 rounded-lg ${location.pathname === "/admin"
+                        className={`text-left px-3 py-2 rounded-lg cursor-pointer ${location.pathname === "/admin"
                             ? "bg-black text-white"
                             : "hover:bg-gray-100 text-gray-600"
                             }`}
                     >
+                        <i className="fa-solid fa-chart-line mr-2"></i>
                         Dashboard
                     </button>
 
                     <button
                         onClick={() => navigate("/admin/products")}
-                        className={`text-left px-3 py-2 rounded-lg ${location.pathname === "/admin/products"
+                        className={`text-left px-3 py-2 rounded-lg cursor-pointer ${location.pathname === "/admin/products"
                             ? "bg-black text-white"
                             : "hover:bg-gray-100 text-gray-600"
                             }`}
                     >
+                        <i className="fa-solid fa-box mr-2"></i>
                         Products
                     </button>
 
                     <button
                         onClick={() => navigate("/admin/orders")}
-                        className={`text-left px-3 py-2 rounded-lg ${location.pathname === "/admin/orders"
+                        className={`text-left px-3 py-2 rounded-lg cursor-pointer ${location.pathname === "/admin/orders"
                             ? "bg-black text-white"
                             : "hover:bg-gray-100 text-gray-600"
                             }`}
                     >
+                        <i className="fa-solid fa-truck mr-2"></i>
                         Orders
                     </button>
                 </div>
