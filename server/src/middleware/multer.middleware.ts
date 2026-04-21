5d0c41851767f8a95840a0b24ad3e5a67c772cad
bizzyumK <bigyamkarmacharya@gmail.com>
2026-04-21 12:37:16 +0545

    feat: cloudinary storage middleware added

--- server/src/middleware/multer.middleware.ts
+++ server/src/middleware/multer.middleware.ts
@@ -1,14 +1,14 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary";
+import multer from "multer";//handle multipart/formdata(file upload)
+import { CloudinaryStorage } from "multer-storage-cloudinary";//custom storage->instead of savin file to disk, it stores in cloudinary
+import cloudinary from "../config/cloudinary";//authenticated connection
 
 //use new keyword because CloudinaryStorage is an class
 const storage = new CloudinaryStorage({
-    cloudinary,
+    cloudinary,//which account to use
     params: {
-        folder: "products",
+        folder: "products",//stores files in folder /products
         allowed_formats: ["jpg", "png", "jpeg"],
     } as any,
 });
 
-export const upload = multer({ storage });
+export const upload = multer({ storage });//this upload object will be used to handle file uploads
